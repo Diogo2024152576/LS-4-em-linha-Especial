a67dec28da1ce31f83e63b4a71f58680c4850784
RaptorD17 <diogofsp1@gmail.com>
2025-04-14 16:25:17 +0100

    Css PopUp Winner

--- projeto/src/components/Jogo1vs1/popUpVencedor/popUpVencedor.jsx
+++ projeto/src/components/Jogo1vs1/popUpVencedor/popUpVencedor.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import './popUpVencedor.css'
 
-export default function PopUpVencedor({winner, player1, player2}) {
-    console.log("vencedor :  " + winner)
+export default function PopUpVencedor({ winner, player1, player2, onReplay, onReset }) {
     return (
         <div className='popup-winner'>
-            <div className='texto'>
-            {winner === -1
-                ? <span>Ninguém venceu (Empate)</span>
-                : <span>O jogador {winner === 1 ? player1 : player2} venceu!</span>
-            }
-            </div>
-            <div className='botoes'>
-                <button onClick={null}>Jogar Novamente</button>
-                <button onClick={null}>Reiniciar Jogo</button>
+            <div className='inner-box'>
+                <div className='texto'>
+                    {winner === -1
+                        ? <span>Ninguém venceu (Empate)</span>
+                        : <span>O jogador <strong>{winner === 1 ? player1 : player2}</strong> venceu!</span>
+                    }
+                </div>
+                <div className='botoes'>
+                    <button onClick={null}>Jogar Novamente</button>
+                    <button onClick={null}>Reiniciar Jogo</button>
+                </div>
             </div>
         </div>
     )
