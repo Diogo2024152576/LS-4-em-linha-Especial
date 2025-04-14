42d44590f3952bcbfce554034407c97212b73026
RaptorD17 <diogofsp1@gmail.com>
2025-04-14 01:02:50 +0100

    Tamanho "header" mudado, double click para dropar a moeda

--- projeto/src/components/Jogo1vs1/Moeda/Moeda.jsx
+++ projeto/src/components/Jogo1vs1/Moeda/Moeda.jsx
@@ -10,6 +10,18 @@ export default function Moeda({ turn, setTurn, dropped, setDropped, hoveredColum
         }
     }, [hoveredColumn]);
 
+    const dropMoeda = () => {
+        if (dropped.find(drop => drop.x === 0 && drop.y === (column || 0))) return;
+
+        const len = 5 - dropped.filter(drop => drop.y === (column || 0)).length;
+
+        setRow(len);
+        setTimeout(() => {
+            setDropped([...dropped, { x: len, y: column || 0, jogador: turn }]);
+            setTurn(turn === 1 ? 2 : 1);
+        }, 500);
+    };
+
     const handleKeyDown = (event) => {
         if ((event.key === 'ArrowLeft' || event.key === 'A' || event.key === 'a') && column > 0) {
             setColumn(column - 1);
@@ -20,15 +32,13 @@ export default function Moeda({ turn, setTurn, dropped, setDropped, hoveredColum
                 setColumn(column + 1);
             }
         } else if (event.key === 'Enter' || event.key === ' ') {
-            if (dropped.find(drop => drop.x === 0 && drop.y === (column || 0))) return;
+            dropMoeda();
+        }
+    };
 
-            const len = 5 - dropped.filter(drop => drop.y === (column || 0)).length;
-
-            setRow(len);
-            setTimeout(() => {
-                setDropped([...dropped, { x: len, y: column || 0, jogador: turn }]);
-                setTurn(turn === 1 ? 2 : 1);
-            }, 500);
+    const handleDoubleClick = (event) => {
+        if (event.button === 0) { // botão esquerdo do rato
+            dropMoeda();
         }
     };
 
@@ -39,14 +49,15 @@ export default function Moeda({ turn, setTurn, dropped, setDropped, hoveredColum
 
     useEffect(() => {
         document.addEventListener('keyup', handleKeyDown, false);
+        document.addEventListener('dblclick', handleDoubleClick, false);
+
         return () => {
             document.removeEventListener('keyup', handleKeyDown, false);
+            document.removeEventListener('dblclick', handleDoubleClick, false);
         };
     });
 
     return (
         <div className={`active p${turn} column-${column || '-'} row-${row === undefined ? '-' : row}`} />
     );
-    
-    
 }
