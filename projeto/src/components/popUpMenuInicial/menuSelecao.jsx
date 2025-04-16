7927ef248635d733616b4145a4fb2ad513ffb370
RaptorD17 <diogofsp1@gmail.com>
2025-04-16 15:33:22 +0100

    Mudanças nos objetivos, banners no menu seleção

--- projeto/src/components/popUpMenuInicial/menuSelecao.jsx
+++ projeto/src/components/popUpMenuInicial/menuSelecao.jsx
@@ -1,12 +1,18 @@
 import './menuSelecao.css'
+import pvspImage from '../../assets/images/pvsp.png'
+import pvspcImage from '../../assets/images/pvspc.png'
 
 export default function MenuSelecaoJogo({startGamePlvsPl, startGamePlvsCmp}) {
     return(
         <div className="selecaoJogada">
             <h1>4 Em Linha Especial</h1>
             <div className="selecao">
-                <button onClick={startGamePlvsPl}>1 vs 1</button>
-                <button onClick={startGamePlvsCmp}>1 vs Computador</button>
+                <button onClick={startGamePlvsPl} className='pvsp'>
+                    <img src={pvspImage} alt="Jogador vs Jogador" />
+                </button>
+                <button onClick={startGamePlvsCmp} className='pvspc'>
+                    <img src={pvspcImage} alt="Jogador vs Computador" />
+                </button>
             </div>
         </div>
     )
